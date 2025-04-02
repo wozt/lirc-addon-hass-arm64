ad31b25ac1ef473f1929d6035dc73eec9193225c
wozt <no@mail.com>
2025-04-02 10:58:02 +0200

    del missplaced runrun.sh

--- lirc-addon-hass/lircd.conf.d/runrun.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Démarrage de LIRC..."
-/usr/sbin/lircd --nodaemon &
-
-# Attendre un instant pour s'assurer que lircd démarre bien
-sleep 10
-
-echo "Démarrage de LIRC Web..."
-exec node /opt/lirc_web/app.js
