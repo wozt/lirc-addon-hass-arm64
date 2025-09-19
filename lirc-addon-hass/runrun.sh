5f992c420dcad460926d2dc0f5b2cb429cccc405
wozt <samuel.courtecuisse@hotmail.fr>
2025-09-19 11:27:46 +0200

    Update runrun.sh

--- lirc-addon-hass/runrun.sh
+++ lirc-addon-hass/runrun.sh
@@ -1,6 +1,18 @@
-#!/bin/sh
+#!/usr/bin/with-contenv bashio
+set -e
+
 echo "Démarrage de LIRC..."
-/usr/sbin/lircd --nodaemon &
+
+DEVICE=$(bashio::config 'device')
+
+if [ -z "$DEVICE" ]; then
+    bashio::log.error "Aucun périphérique sélectionné. Configurez l'add-on avant de le démarrer."
+    exit 1
+fi
+
+bashio::log.info "Utilisation du périphérique IR: $DEVICE"
+
+/usr/sbin/lircd --nodaemon --device="$DEVICE" &
 
 # Attendre un instant pour s'assurer que lircd démarre bien
 sleep 10
