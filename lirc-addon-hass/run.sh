3e794fc7ea1db97c11a6938dcec5f86f958647e8
wozt <samuel.courtecuisse@hotmail.fr>
2025-09-19 14:18:23 +0200

    Update run.sh

--- lirc-addon-hass/run.sh
+++ lirc-addon-hass/run.sh
@@ -1,7 +1,7 @@
 docker run -d \
   --name lirc-addon-test \
   --privileged \
-  --device /dev/bus/usb/001/004:/dev/bus/usb/001/004:rwm \
+  --device /dev/bus/usb/001/003:/dev/bus/usb/001/004:rwm \
   -p 8765:8765 \
   -p 3000:3000 \
   lirc-addon-hass-arm64
